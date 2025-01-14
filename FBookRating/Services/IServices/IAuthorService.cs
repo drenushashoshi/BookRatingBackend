cd15c1f83b147dc3a6c49b78782497e3d29cbc45
rronhaxhiu <rronihhh@gmail.com>
2025-01-14 23:58:37 +0100

    added service layer 2

---
+++ FBookRating/Services/IServices/IAuthorService.cs
@@ -0,0 +1,17 @@
+﻿using FBookRating.Models.DTOs.Author;
+using FBookRating.Models.Entities;
+
+namespace FBookRating.Services.IServices
+{
+    public interface IAuthorService
+    {
+        Task<IEnumerable<AuthorReadDTO>> GetAllAuthorsAsync();
+        Task<AuthorReadDTO> GetAuthorByIdAsync(int id);
+        Task AddAuthorAsync(AuthorCreateDTO authorDTO);
+
+        /*
+        Task UpdateAuthorAsync(int id, Author author);
+        Task DeleteAuthorAsync(int id);
+        */
+    }
+}
